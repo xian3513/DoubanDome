4194518bc8b7bcecbcbe0972ce884d907dd7c791
zzx <23090284@qq.com>
2015-11-27 11:59:05 +0800

    first commit

---
+++ DoubanDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoubanDome
+//
+//  Created by xian on 15/11/27.
+//  Copyright © 2015年 xian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoubanDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DoubanDome
+//
+//  Created by xian on 15/11/27.
+//  Copyright © 2015年 xian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
